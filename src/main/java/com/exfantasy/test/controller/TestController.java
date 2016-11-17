1ad8b53502a3e72d30aec4c7caf12605d080cec2
TommyYehCool <tommy.yeh1112@gmail.com>
2016-11-17 12:31:40 +0800

    Refactoring

--- src/main/java/com/exfantasy/test/controller/TestController.java
+++ src/main/java/com/exfantasy/test/controller/TestController.java
@@ -6,47 +6,47 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.stage.Stage;
-import javafx.scene.control.Label;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class TestController implements Initializable {
 	// ---------------- FX Related ----------------
 	private Stage stage;
 
-	@FXML 
-	private Label lblProcessResult;
+	@FXML
+	private DatePicker dpConsumeDate;
 
-	@FXML 
-	private Button btnClear;
-
-	@FXML 
-	private Button btnQuery;
-
-	@FXML 
-	private Button btnInsert;
-
-	@FXML 
-	private TableView tvConsumeDatas;
-
-	@FXML 
-	private TextField tfdLotteryNo;
-
-	@FXML 
-	private TextField tfdAmount;
-
-	@FXML 
-	private TextField tfdProdName;
-
-	@FXML 
+	@FXML
 	private ComboBox cmbTypes;
 
-	@FXML 
-	private DatePicker dpConsumeDate;
+	@FXML
+	private TextField tfdProdName;
+
+	@FXML
+	private TextField tfdAmount;
+
+	@FXML
+	private TextField tfdLotteryNo;
+
+	@FXML
+	private TableView tvConsumeDatas;
+
+	@FXML
+	private Button btnInsert;
+
+	@FXML
+	private Button btnQuery;
+
+	@FXML
+	private Button btnClear;
+
+	@FXML
+	private Label lblProcessResult;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -56,7 +56,7 @@ public class TestController implements Initializable {
 	public void setStage(Stage stage) {
 		this.stage = stage;
 	}
-	
+
 	private void setAquaFxStyle() {
 	}
 
@@ -64,11 +64,9 @@ public class TestController implements Initializable {
 	public void handleButtonsAction(ActionEvent event) {
 		if (event.getSource().equals(btnInsert)) {
 			System.out.println("Insert button clicked...");
-		}
-		else if (event.getSource().equals(btnQuery)) {
+		} else if (event.getSource().equals(btnQuery)) {
 			System.out.println("Query button clicked...");
-		}
-		else if (event.getSource().equals(btnClear)) {
+		} else if (event.getSource().equals(btnClear)) {
 			System.out.println("Clear button clicked...");
 		}
 	}
