8f9fe705c3cb5e4d697f6cbbff8eeb881ce7b013
TommyYehCool <tommy.yeh1112@gmail.com>
2016-11-18 17:05:18 +0800

    處裡錯誤訊息顯示

--- src/main/java/com/exfantasy/test/controller/TestController.java
+++ src/main/java/com/exfantasy/test/controller/TestController.java
@@ -17,10 +17,10 @@ import com.exfantasy.test.config.ConfigHolder;
 import com.exfantasy.test.enu.DataType;
 import com.exfantasy.test.enu.TableColDef;
 import com.exfantasy.test.enu.Type;
-import com.exfantasy.test.testcase.TestCase;
 import com.exfantasy.test.vo.Consume;
 import com.exfantasy.utils.http.HttpUtil;
 import com.exfantasy.utils.http.HttpUtilException;
+import com.google.gson.Gson;
 
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
@@ -36,6 +36,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class TestController implements Initializable {
@@ -89,7 +90,9 @@ public class TestController implements Initializable {
 		
 		loadConfig();
 		
-		loginToSever();
+		new Thread(() -> {
+			loginToSever();
+		}).start();;
 	}
 	
 	private void setAquaFxStyle() {
@@ -97,11 +100,30 @@ public class TestController implements Initializable {
 	}
 	
 	private void initComponents() {
+		changeButtonsStateByIsFxAppThread(true);
+		
 		fillTypeCombos();
 		
 		initTable();
 	}
 	
+	private void changeButtonsStateByIsFxAppThread(boolean disable) {
+		boolean isFxAppThread = Platform.isFxApplicationThread();
+		if (isFxAppThread) {
+		    changeButtonsState(disable);
+		}
+		else {
+		    Platform.runLater(() -> {
+		    	changeButtonsState(disable);
+		    });
+		}
+    }
+	
+	private void changeButtonsState(boolean disable) {
+		btnInsert.setDisable(disable);
+		btnQuery.setDisable(disable);
+	}
+
 	private void loadConfig() {
 		mConfig = ConfigHolder.getInstance().getConfig();
 	}
@@ -114,8 +136,10 @@ public class TestController implements Initializable {
 		params.add(new BasicNameValuePair("password", "1234qwer"));
 		try {
 			HttpUtil.sendPostRequest(url, params);
+			changeButtonsState(false);
 		} catch (HttpUtilException e) {
 			logger.error("Try to login to url: <{}> failed", url, e);
+			showErrorMsg("嘗試登入伺服器失敗, 網址: " + mConfig.getHost());
 		}
 	}
 
@@ -181,12 +205,22 @@ public class TestController implements Initializable {
 	@FXML
 	public void handleButtonsAction(ActionEvent event) {
 		if (event.getSource().equals(btnInsert)) {
-			makeInsert();
+			if (checkInput()) {
+				makeInsert();
+			}
 		} else if (event.getSource().equals(btnQuery)) {
 			makeQuery();
 		} else if (event.getSource().equals(btnClear)) {
 			makeClear();
 		}
+	}
+
+	private boolean checkInput() {
+		LocalDate date = dpConsumeDate.getValue();
+		if (date == null) {
+			return false;
+		}
+		return true;
 	}
 
 	private void makeInsert() {
@@ -197,10 +231,18 @@ public class TestController implements Initializable {
 		String lotteryNo = tfdLotteryNo.getText();
 		
 		Consume consume = new Consume(consumeDate, type, prodName, amount, lotteryNo);
-		
-		// TODO insert somewhere
-		
-		mConsumes.add(consume);
+		try {
+			sendAddConsume(consume);
+			mConsumes.add(consume);
+		} catch (HttpUtilException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void sendAddConsume(Consume consume) throws HttpUtilException {
+		final String url = mConfig.getHost() + "/addConsume";
+		String jsonData = new Gson().toJson(consume);
+		HttpUtil.sendPostRequest(url, jsonData);
 	}
 
 	private void makeQuery() {
@@ -209,5 +251,26 @@ public class TestController implements Initializable {
 
 	private void makeClear() {
 		mConsumes.clear();
+	}
+	
+	private void showMsg(String msg) {
+		showMsg(false, msg);
+	}
+	
+	private void showErrorMsg(String msg) {
+		showMsg(true, msg);
+	}
+	
+	private void showMsg(boolean isError, String msg) {
+		boolean isFxAppThread = Platform.isFxApplicationThread();
+		if (isFxAppThread) {
+			lblProcessResult.setText(msg);
+			lblProcessResult.setTextFill(isError ? Color.RED : Color.BLACK);
+		} else if (!isFxAppThread) {
+			Platform.runLater(() -> {
+				lblProcessResult.setText(msg);
+				lblProcessResult.setTextFill(isError ? Color.RED : Color.BLACK);
+			});
+		}
 	}
 }
