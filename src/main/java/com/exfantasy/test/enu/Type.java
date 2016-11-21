b651ac278ad422eb7a46c291f50c60fcea68c164
TommyYehCool <tommy.yeh1112@gmail.com>
2016-11-21 14:20:56 +0800

    調整商品類別

--- src/main/java/com/exfantasy/test/enu/Type.java
+++ src/main/java/com/exfantasy/test/enu/Type.java
@@ -5,11 +5,10 @@ public enum Type {
 	BREAKFAST(1, "早餐"),
 	LUNCH(2, "中餐"),
 	DINNER(3, "晚餐"),
-	DRINK(4, "飲料"),
-	CIGARETTE(5, "香菸"),
-	GAS(6, "加油"),
-	PARKING(7, "停車費"),
-	PLAY(8, "娛樂");
+	DRINK(4, "7-11"),
+	GAS(5, "加油"),
+	PARKING(6, "停車費"),
+	PLAY(7, "娛樂");
 	
 	private int code;
 	private String description;
