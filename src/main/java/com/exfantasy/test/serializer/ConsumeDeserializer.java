c3ea11c5befa6c1eb9b510f7c562782624e89824
TommyYehCool <tommy.yeh1112@gmail.com>
2016-11-22 18:25:26 +0800

    Add FIXME

--- src/main/java/com/exfantasy/test/serializer/ConsumeDeserializer.java
+++ src/main/java/com/exfantasy/test/serializer/ConsumeDeserializer.java
@@ -1,7 +1,12 @@
 package com.exfantasy.test.serializer;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
 
+import com.exfantasy.test.enu.Type;
 import com.exfantasy.test.vo.Consume;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -18,6 +23,9 @@ public class ConsumeDeserializer extends JsonDeserializer<Consume> {
 	    JsonNode node = oc.readTree(jp);
 		
 	    long lConsumeDate = node.get("consumeDate").asLong();
+	    // FIXME 處理 LocalDate 轉換錯誤問題
+	    LocalDate consumeDate = getDateFromTimestamp(lConsumeDate);
+	    
 	    int type = node.get("type").asInt();
 	    String prodName = node.get("prodName").asText();
 	    int amount = node.get("amount").asInt();
@@ -25,7 +33,26 @@ public class ConsumeDeserializer extends JsonDeserializer<Consume> {
 	    boolean got = node.get("got").asBoolean();
 	    int prize = node.get("prize").asInt();
 	    
-		return null;
+	    Consume consume = new Consume();
+	    consume.setConsumeDate(consumeDate);
+	    consume.setType(Type.convert(type));
+	    consume.setProdName(prodName);
+	    consume.setAmount(amount);
+	    consume.setLotteryNo(lotteryNo);
+	    consume.setGot(got);
+	    consume.setPrize(prize);
+	    
+		return consume;
 	}
 
+	public LocalDateTime getDateTimeFromTimestamp(long timestamp) {
+		if (timestamp == 0)
+			return null;
+		return LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp), TimeZone.getDefault().toZoneId());
+	}
+
+	public LocalDate getDateFromTimestamp(long timestamp) {
+		LocalDateTime date = getDateTimeFromTimestamp(timestamp);
+		return date == null ? null : date.toLocalDate();
+	}
 }
